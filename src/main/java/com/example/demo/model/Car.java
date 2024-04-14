4508c2362dc02921043b82b39e55452caecb8de9
Lennon Machado da Silva <lennon2049@gmail.com>
2024-04-13 21:22:47 -0300

    feat: Adiciona Tipo Enum

--- src/main/java/com/example/demo/model/Car.java
+++ src/main/java/com/example/demo/model/Car.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -18,15 +20,17 @@ import lombok.Data;
 @Entity(name = "cars")
 @Table(name = "cars")
 public class Car {
-    
-    public Car(String name, String color, String model, int car_year) {
+
+    public Car(String name, String color, String model, int car_year, FuelType fuelType) {
         this.name = name;
         this.color = color;
         this.model = model;
         this.car_year = car_year;
+        this.fuelType = fuelType;
     }
 
-    public Car() {}
+    public Car() {
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -50,4 +54,8 @@ public class Car {
     @NotNull
     @Positive
     private int car_year;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private FuelType fuelType;
 }
---
+++ src/main/java/com/example/demo/model/FuelType.java
@@ -0,0 +1,5 @@
+package com.example.demo.model;
+
+public enum FuelType {
+    GASOLINE, DIESEL, ELECTRIC, HYBRID, UNKNOWN
+}
